e3744350f28de58835f72af20f4d7f8f821a5f6d
Timothée Gauthier <t.gauthier42@gmail.com>
2019-05-08 10:57:41 +0200

    Generate 2 pictures

--- test_png.clj
+++ test_png.clj
@@ -7,16 +7,17 @@
 (def bi (BufferedImage. 500 500 BufferedImage/TYPE_INT_ARGB))
 (def g (.createGraphics bi))
 
-(defn run []
+(defn run [position name]
   (.drawLine g 0 0 500 500)
   (.drawLine g 0 500 500 0)
-  (.drawLine g 125 0 125 500)
-  (ImageIO/write bi "png"  (File. "pictures/test.png"))
+  (.drawLine g (+ 125 position) 0 125 500)
+  (ImageIO/write bi "png"  (File. (str "pictures/" "test" name ".png")))
   )
 
 
 (defn -main [& args]
-  (run)
+  (run 0 "01")
+  (run 100 "02")
   (println "please run :")
-  (println "xdg-open pictures/test.png")
+  (println "xdg-open pictures")
   )
